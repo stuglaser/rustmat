f25e6b5cabca4626672d61a609882bd7fa8e9be5
Stuart Glaser <stuglaser@gmail.com>
2014-12-18 15:31:57 -0800

    One round of QR working

--- src/qr.rs
+++ src/qr.rs
@@ -14,6 +14,9 @@ impl QR {
         let mut R = A.clone();
         let mut Q = Mat::ident(A.r);
 
+        println!("inital Q:\n{}", Q);
+        println!("inital R:\n{}", R);
+
 
         let mut e1 = Mat::zero(R.rows(), 1);
 
@@ -21,18 +24,35 @@ impl QR {
         let w = {
             let x = R.col(0);
             e1[(0, 0)] = x.norm();
+            println!("|x| * e = \n{}", e1);
             let mut v = x - e1;
+            println!("v = \n{}", v);
             v.normalize();
             v
         };
 
+        println!("w = \n{}", w);
+
+        let H = Mat::ident(R.rows()) - &w * &w.t() * 2.0;
+        println!("H = \n{}", H);
+
         // H := I - 2ww'
         // R := H * R
-        let tmp = &w.t() * &R;  // Borrow checker limitations
+        let tmp = &w.t() * &R;  // Borrow checker limitations (for R)
         R.sub_assign(&w * tmp * 2.0);
-
         //R.block_lr() -= 2 * v * (v.t() * R.block_lr());
 
+        // Q := Q * H
+        let tmp = &Q * &w;
+        Q.sub_assign(tmp * w.t() * 2.0);
+
+        println!("Q after:\n{}", Q);
+        println!("R after:\n{}", R);
+        println!("Q*R after:\n{}", &Q * &R);
+
+
+        // A = QR
+        // Q' * A = R
         
         
         QR{Q: Q, R: R}
@@ -52,4 +72,12 @@ fn test_qr_simple() {
     let R = Mat::from_slice(3, 3, &[1.0, 1.0, 2.0,
                                     0.0, 1.0, 1.0,
                                     0.0, 0.0, 3.0]);
+}
+
+#[test]
+fn test_qr_wikipedia() {
+    let A = Mat::from_slice(3, 3, &[12.0, -51.0, 4.0,
+                                    6.0, 167.0, -68.0,
+                                    -4.0, 24.0, -41.0]);
+    let qr = QR::of(&A);
 }
